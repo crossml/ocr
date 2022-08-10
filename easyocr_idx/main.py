e79cbab5762837d9dbf12d0af7b55014dfbdce86
Yakul-crossml <yakul@crossml.com>
2022-08-10 11:45:38 +0530

    feat: add local save file
    Add constructor.
    Add doc string.
    Add local save path

--- easyocr_idx/main.py
+++ easyocr_idx/main.py
@@ -3,6 +3,7 @@ easyocr pipline
 """
 import json
 import os
+import shutil
 from zipfile import ZipFile
 import easyocr
 from pdf2image import convert_from_path
@@ -10,7 +11,6 @@ import boto3
 from PIL import Image
 from config import EXTENSION_LISTS
 from config import TEMP
-from config import S3_BUCKET_NAME
 from PIL import Image, ImageSequence
 
 
@@ -19,29 +19,44 @@ SESSION = boto3.Session()
 S3 = SESSION.resource('s3')
 
 
-def upload_file_to_s3(json_path):
+def upload_file_to_s3(json_path,storage_path):
     """
-    Upload File to s3
+    Upload image and relative json file to aws s3 cloud.
+
+    Args:
+        json_path (dict): path where file is store on s3.
+        storage_path (string): name of s3 bucket.
     """
     try:
         for file in os.listdir(json_path):
             s3_json_path = os.path.join(json_path, file)
             S3.meta.client.upload_file(
-                s3_json_path, S3_BUCKET_NAME, s3_json_path)
+                s3_json_path, storage_path, os.path.join('easyocr_output',os.path.basename(json_path),file))
     except Exception as error:
+        print(error)
         return error
 
 
-class Easyocrpipleline:
+class EasyOcrProcessor:
     """
-    Easy ocr pipeline
+    Easy ocr pipeline for images pdf tif jpef zip file read
     """
+    def __init__(self, config):
+        self.config=config
 
     def create_json(self, result, file):
         """
-        json file save
+        Function for create json of ocr result
+
+        1. Create dictionary of result ocr in proper format.
+        2. Save the dictionary in file with the name of relative image.
+
+        Args:
+            result (dict): dictionary of result
+            file (string): name of file
         """
         try:
+            
             dictionary = {}
             # create proper json to store in json file
             dictionary = [{'left': int(i[0][0][0]),
@@ -58,15 +73,24 @@ class Easyocrpipleline:
         except Exception as error:
             return error
 
+
     def image_read(self, path, images):
         """
-        Read the image and give text
+        Function for create image path and upload the file in s3
+
+        1. Make folder of image name.
+        2. save image in relative folder for each image
+        3. read text from each image and store json in relative folder
+        4. upload image and json file on s3
 
         Args:
-            path (string): _description_
-            images (object): _description_
+            path (string): path of image.
+            images (object): object of image.
+            storage_type (string): type of storage where file is store.
+            storage_path (string): path of storage where file is store.
         """
         try:
+            reader = easyocr.Reader(['hi', 'en'])
             path = os.path.basename(path)
             # get file name
             file_name = os.path.splitext(path)[0]
@@ -86,23 +110,28 @@ class Easyocrpipleline:
                 file_path = os.path.join(folder_path, file_path)
                 # save image
                 img.save(file_path)
-                reader = easyocr.Reader(['hi', 'en'])
                 # read the image data
                 result = reader.readtext(file_path, width_ths=0)
                 # function to create json
                 self.create_json(result, file_path)
-            # upload file into s3
-            upload_file_to_s3(folder_path)
+            if self.config.get('storage_type').lower()=='aws':
+                # upload file into s3
+                upload_file_to_s3(folder_path, self.config.get('storage_path'))
+            elif self.config.get('storage_type').lower()=='local':
+                shutil.copytree(folder_path, os.path.join(self.config.get('storage_path'),file_name), symlinks=False, ignore=None, ignore_dangling_symlinks=False, dirs_exist_ok=True)
         except Exception as error:
+            print(error)
             return error
 
-    def image_process(self, path):
+    # def image_process(self, path):
+    def process_image(self, path):
         """
         Image process
 
         Args:
             path (string): file path
-            reader (object): easy ocr object
+            storage_type (string): type of storage where file is store.
+            storage_path (string): path of storage where file is store.
         """
         try:
             img = Image.open(path)
@@ -110,15 +139,21 @@ class Easyocrpipleline:
             # read the image
             self.image_read(path, images)
         except Exception as error:
+            print(error)
             return error
 
-    def pdf_process(self, path):
+    # def pdf_process(self, path):
+    def process_pdf(self, path):
         """
-        PDF processing
+        Process the pdf file
+
+        1. convert each page of pdf into image.
+        2. pass images to image read function to get text from each image.
 
         Args:
             path (string): file path
-            reader (object): easy ocr object
+            storage_type (string): type of storage where file is store.
+            storage_path (string): path of storage where file is store.
         """
         try:
             # convert the pdf into images
@@ -126,15 +161,21 @@ class Easyocrpipleline:
             # read the image
             self.image_read(path, images)
         except Exception as error:
+            print(error)
             return error
 
-    def zip_process(self, path):
+    def process_zip(self, path):
         """
-        zip processing method
+        Process the zip file
+
+        1. Extract each file in zip one by one.
+        2. If file is of pdf type than file is pass in process_pdf function for further process.
+        3. If file is of image type than it is pass in process_image function for further process.
 
         Args:
             path (string): file path
-            reader (object): easy ocr object
+            storage_type (string): type of storage where file is store.
+            storage_path (string): path of storage where file is store.
         """
         try:
             # read the zip file
@@ -143,10 +184,13 @@ class Easyocrpipleline:
                     zip_file.extract(file, TEMP)
                     extension = os.path.splitext(file)[-1].lower()
                     if extension in EXTENSION_LISTS:
-                        self.image_process(TEMP+file)
+                        self.process_image(TEMP+file)
                     elif extension == '.pdf':
-                        self.pdf_process(TEMP+file)
+                        self.process_pdf(TEMP+file)
                     else:
                         return "Invalid Extension"
         except Exception as error:
+            print(error)
             return error
+
+
